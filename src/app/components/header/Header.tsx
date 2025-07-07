ad3cb88be25cf06d995fe1387b7eaeb4addd8cf4
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-07-07 23:34:42 +0200

    Dev: Making header responsive part1

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -4,13 +4,24 @@ import Link from "next/link";
 import styles from './header.module.css'
 import { ArrowTrendingUpIcon, AtSymbolIcon, HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { SparklesIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
+  const isMenuOpen:boolean = true;
   return (
     <nav className={styles.headerContainer}>
-      <Link href="/">
-        <Image  src={logo} alt="Community banner and clan emblem" quality={80} width={100} placeholder="blur" />
-      </Link>
+      <div className={styles.headerTitle}>
+        <Link href="/">
+          <Image  src={logo} alt="Community banner and clan emblem" quality={80} width={100} placeholder="blur" />
+        </Link>
+        <div className={styles.burgerContainer}>
+          {
+            isMenuOpen ? 
+              <XMarkIcon width={36} cursor="pointer" color="#383838" />
+              :<Bars3Icon width={36} cursor="pointer" color="#383838" />
+          }
+        </div>
+      </div>
       <ul>
         <li>
           <Link href="/">
