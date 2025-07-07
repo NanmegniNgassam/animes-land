ed8c6eee5931d0851218c5e5e3f56cc0000f6aea
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-07-07 23:49:00 +0200

    Dev: Setting the header responsive

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import logo from '../../assets/animes-land_logo.png'
 import Link from "next/link";
@@ -5,9 +6,13 @@ import styles from './header.module.css'
 import { ArrowTrendingUpIcon, AtSymbolIcon, HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const Header = () => {
-  const isMenuOpen:boolean = true;
+  const [isMenuOpen, setMenuOpen] = useState<boolean>(true);
+
+  const toggleMenu = () => setMenuOpen(!isMenuOpen);
+
   return (
     <nav className={styles.headerContainer}>
       <div className={styles.headerTitle}>
@@ -17,43 +22,47 @@ const Header = () => {
         <div className={styles.burgerContainer}>
           {
             isMenuOpen ? 
-              <XMarkIcon width={36} cursor="pointer" color="#383838" />
-              :<Bars3Icon width={36} cursor="pointer" color="#383838" />
+              <XMarkIcon width={36} cursor="pointer" color="#383838" onClick={toggleMenu} />
+              : <Bars3Icon width={36} cursor="pointer" color="#383838" onClick={toggleMenu}  />
           }
         </div>
       </div>
-      <ul>
-        <li>
-          <Link href="/">
-            <HomeIcon width={24} />
-            <span>Home</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/animes">
-            <SparklesIcon width={24} />
-            <span>Animes</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/top">
-            <ArrowTrendingUpIcon width={24} />
-            <span>Top animes</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/search">
-            <MagnifyingGlassIcon width={24} />
-            <span>Search</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <AtSymbolIcon width={24} />
-            <span>About</span>
-          </Link>
-        </li>
-      </ul>
+      {
+        isMenuOpen && (
+          <ul>
+            <li>
+              <Link href="/">
+                <HomeIcon width={24} />
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/animes">
+                <SparklesIcon width={24} />
+                <span>Animes</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/top">
+                <ArrowTrendingUpIcon width={24} />
+                <span>Top animes</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/search">
+                <MagnifyingGlassIcon width={24} />
+                <span>Search</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <AtSymbolIcon width={24} />
+                <span>About</span>
+              </Link>
+            </li>
+          </ul>
+        )
+      }
     </nav>
   );
 }
