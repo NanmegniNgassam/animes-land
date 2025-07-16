752dab81724b989421f16c22678335d6931d94f5
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-07-16 20:48:19 +0200

    Dev: Structuring home herosection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { Metadata } from "next"
 import styles from "./page.module.css"
+import Image from "next/image"
+import logo from './assets/animes-land_logo.png'
+import naruto from './assets/naruto.jpg'
+import onePiece from './assets/one_piece.jpg'
+import tokyoRevengers from './assets/tokyo_revengers.jpg'
+
 
 export const metadata: Metadata = {
   title: 'Animes-land | Home',
@@ -12,10 +18,21 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main>
-      <h1>Animes-land</h1>
-      <p className={styles.paragraph}>
-        Animes-land est un micro-projet conçu pour visualiser des animés populaires, explorer leurs détails, et rechercher ses séries favorites. Il a pour objectif de consolider mes compétences en Next.js, en particulier autour de la récupération de données, la navigation dynamique, et les bonnes pratiques de développement frontend moderne.
-      </p>
+      <div className={styles.heroSection}>
+        <div className={styles.title}>
+          <h1>Animes-land</h1>
+          <span className={styles.subtitle}>Explorez l’univers des animés</span>
+        </div>
+        
+        <p className={styles.paragraph}>
+          Bienvenue sur Animes Land, ton nouveau repaire pour explorer l’univers des animés japonais. Que tu sois passionné de shōnen, amateur de récits tranchants ou curieux de découvrir les perles de la saison, cette plateforme t’ouvre les portes d’un monde riche en émotions, en aventures et en chefs-d’œuvre visuels. Recherche, découvre, et plonge dans des centaines de titres avec simplicité et passion. Ici, chaque animé a une histoire — à toi de la vivre.
+        </p>
+        <div className={styles.pillsContainer}>
+          <Image src={onePiece} alt="Couverture de l'animé One Piece" quality={100} className={styles.pillWrapper} placeholder="blur" />
+          <Image src={naruto} alt="Couverture de l'animé Naruto" quality={100}  className={styles.pillWrapper} placeholder="blur" />
+          <Image src={tokyoRevengers} alt="Couverture de l'animé Tokyo Revengers" quality={100}  className={styles.pillWrapper} placeholder="blur" />
+        </div>
+      </div>
     </main>
   )
 }
