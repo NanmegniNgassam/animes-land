1c3d642ae689905c832a37a7e573e1130018bac5
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-08-04 12:03:43 +0200

    Dev: Retrieving animes from the api

--- src/app/components/animeCard/AnimeCard.tsx
+++ src/app/components/animeCard/AnimeCard.tsx
@@ -1,6 +1,23 @@
+'use client'
+import { useEffect } from 'react';
 import styles from './animeCard.module.css'
+import { Anime } from '@/app/models/anime';
+
+
 
 const AnimeCard = () => {
+  useEffect(() => {
+    const getAnime = async () => {
+      const response = await fetch('https://api.jikan.moe/v4/anime/52990');
+      const jsonResponse: { data: Anime } = await response.json();
+
+      const anime = jsonResponse.data;
+      console.log('Fetched anime : ', anime);
+    };
+
+    getAnime();
+  }, [])
+
   return (
     <div className={styles.animeCardWrapper}>
       AnimeCard
