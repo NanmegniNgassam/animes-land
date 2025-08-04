1099cb27f0c7743674c2f6fba71da0c8473704dd
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-08-04 11:51:14 +0200

    Dev: Making up a model for animes

---
+++ src/app/models/anime.ts
@@ -0,0 +1,27 @@
+export interface Anime {
+  mal_id: number;
+  url: string;
+  approved: boolean;
+  titles: {type: string; title: string}[];
+  title: string;
+  type: string | "TV";
+  source: string;
+  episodes: number;
+  status: string;
+  airing: boolean;
+  aired: {
+    from: string;
+    to: string;
+  };
+  duration: string;
+  rating: string;
+  score: number;
+  score_by: number;
+  rank: number;
+  popularity: number;
+  synopsis: string;
+  season: "fall" | "winter" | "spring" | "summer";
+  genres: {mal_id: number; type: string; name: string; url: string}[];
+  explicit_genres:  {mal_id: number; type: string; name: string; url: string}[];
+  themes: {mal_id: number; type: string; name: string; url: string}[]
+}
